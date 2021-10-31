5858b0b7a51d9f9a68f91a35677a4dc18f6169d8
GabrielSanga <gabrie.sanga.fef@gmail.com>
2021-10-31 19:38:39 -0300

    Adicionado Programação Defensiva na model Negociacao

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,22 +1,10 @@
 export class Negociacao {
 
     constructor(
-        private _data: Date,
-        private _quantidade: number,
-        private _valor: number
+        public readonly _data: Date,
+        public readonly _quantidade: number,
+        public readonly _valor: number
     ){}
-
-    get data(): Date{
-        return this._data;
-    }
-
-    get quantidade(): number{
-        return this._quantidade;
-    }
-
-    get valor(): number{
-        return this._valor;
-    }
 
     get volume(): number{
         return this._valor * this._quantidade 
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -4,15 +4,6 @@ export class Negociacao {
         this._quantidade = _quantidade;
         this._valor = _valor;
     }
-    get data() {
-        return this._data;
-    }
-    get quantidade() {
-        return this._quantidade;
-    }
-    get valor() {
-        return this._valor;
-    }
     get volume() {
         return this._valor * this._quantidade;
     }
